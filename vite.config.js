e447fe953b76719eb0380c980160b3d8ff871b2c
ValkinDED <sonyerik289@gmail.com>
2025-05-02 16:17:32 +0200

    CV

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {},
+  },
   build: {
     sourcemap: true,
-    outDir: "dist",
-  },
-  base: "/CV/", // обязательно: имя репозитория
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "./src"),
-    },
   },
 });
